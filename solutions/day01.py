f6ffa8243c9ad8ecc604c1c73f2fb7583d4cb95b
NicoloMarsucco <nicolomarsucco@gmail.com>
2025-12-02 17:21:46 +0000

    Improve variables naming

--- solutions/day01.py
+++ solutions/day01.py
@@ -3,35 +3,35 @@ from solutions.utils import load_input
 def part1(data: str) -> int:
     commands = data.splitlines()
     res = 0
-    dial_angle = 50
+    curr_angle = 50
     for command in commands:
         if command[0] == 'L':
-            dial_angle -= int(command[1:])
+            curr_angle -= int(command[1:])
         else:
-            dial_angle += int(command[1:])
+            curr_angle += int(command[1:])
 
-        if dial_angle % 100 == 0:
+        if curr_angle % 100 == 0:
             res += 1
     return res
 
 def part2(data: str) -> int:
     commands = data.splitlines()
     res = 0
-    dial_angle = 50
+    curr_angle = 50
     for command in commands:
         if command[0] == 'L':
-            delta = -int(command[1:])
+            delta_angle = -int(command[1:])
         else:
-            delta = int(command[1:])
+            delta_angle = int(command[1:])
         
-        next_angle = dial_angle + delta
-        res += abs(dial_angle // 100 - next_angle // 100)
-        if dial_angle % 100 == 0 and delta < 0:
+        next_angle = curr_angle + delta_angle
+        res += abs(curr_angle // 100 - next_angle // 100)
+        if curr_angle % 100 == 0 and delta_angle < 0:
             res -= 1
-        if next_angle % 100 == 0 and delta < 0:
+        if next_angle % 100 == 0 and delta_angle < 0:
             res += 1
 
-        dial_angle = next_angle
+        curr_angle = next_angle
     return res
 
 if __name__ == "__main__":
